f29115bdcbcd5911745b3ca9df4352f255d72745
Marvie Solis <marvie_solis@ymail.com>
2018-05-30 18:06:11 -0700

    Added a related movies module using OMDB and updated README.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,7 +8,7 @@ var limit = 10;
 // Generate initial buttons on page
 function generateButtons() {
     $("#gifButtons").empty();
-    for (var i=0; i < topics.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
         var gifButton = $("<button>");
         gifButton.attr("data-name", topics[i]);
         gifButton.text(topics[i]);
@@ -20,7 +20,7 @@ function generateButtons() {
 
 // Create new button from user entry
 function newButton() {
-    $("#submitButton").on("click", function() {
+    $("#submitButton").on("click", function () {
         var topic = $("#userEntry").val().trim();
         var check = topics.indexOf(topic);
         if (topic === "") {
@@ -38,21 +38,57 @@ function newButton() {
     });
 }
 
+
+// Display related movies based on chosen topic
+function displayMovies() {
+    var queryURL = "http://www.omdbapi.com/?s=" + currentTopic + "&apikey=trilogy";
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .done(function (response) {
+            console.log(response);
+            $("#movies").empty();
+            var results = response.Search;
+            if (results === "") {
+                alert("Sorry, no movies can be retrieved regarding this topic ):");
+            }
+            for (var i = 0; i < 3; i++) {
+
+                var movie = $("<div style='float:left'</div>");
+                $(movie).addClass("movieBox");
+
+                var movieImage = $("<img>");
+                $(movieImage).attr("src", results[i].Poster);
+                $(movieImage).attr("height", "300px");
+                $(movieImage).addClass("selectionMovie");
+                $(movie).append(movieImage);
+
+                var movieTitle = $("<p>").text(results[i].Title);
+                movie.append(movieTitle);
+
+                $("#movies").prepend(movie);
+            }
+        })
+}
+
+
 // Change amount of GIFs displayed
 function changeLimit() {
-    $("#limit10").on("click", function() {
+    $("#limit10").on("click", function () {
         limit = 10;
         generateGifs2();
         return false;
     });
 
-    $("#limit15").on("click", function() {
+    $("#limit15").on("click", function () {
         limit = 15;
         generateGifs2();
         return false;
     });
 
-    $("#limit20").on("click", function() {
+    $("#limit20").on("click", function () {
         limit = 20;
         generateGifs2();
         return false;
@@ -70,32 +106,32 @@ function generateGifs() {
         url: queryURL,
         method: "GET"
     })
-    .done(function(response) {
-        console.log(response);
-        $("#gifDisplay").empty();
-        var results = response.data;
-        if (results === "") {
-            alert("Sorry, no GIFs can be retrieved regarding this topic ):");
-        }
-        for (var i=0; i<results.length; i++) {
+        .done(function (response) {
+            console.log(response);
+            $("#gifDisplay").empty();
+            var results = response.data;
+            if (results === "") {
+                alert("Sorry, no GIFs can be retrieved regarding this topic ):");
+            }
+            for (var i = 0; i < results.length; i++) {
 
-            var gif = $("<div style='float:left'</div>");
-            $(gif).addClass("gifBox");
+                var gif = $("<div style='float:left'</div>");
+                $(gif).addClass("gifBox");
 
-            var gifImage = $("<img>");
-            $(gifImage).attr("src", results[i].images.fixed_height_small_still.url);
-            $(gifImage).attr("data-still", results[i].images.fixed_height_small_still.url);
-            $(gifImage).attr("data-animate", results[i].images.fixed_height_small.url);
-            $(gifImage).attr("data-state", "data-still");
-            $(gifImage).addClass("selection");
-            $(gif).append(gifImage);
+                var gifImage = $("<img>");
+                $(gifImage).attr("src", results[i].images.fixed_height_small_still.url);
+                $(gifImage).attr("data-still", results[i].images.fixed_height_small_still.url);
+                $(gifImage).attr("data-animate", results[i].images.fixed_height_small.url);
+                $(gifImage).attr("data-state", "data-still");
+                $(gifImage).addClass("selection");
+                $(gif).append(gifImage);
 
-            var gifRating = $("<p>").text("Rating: " + results[i].rating);
-            gif.append(gifRating);
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gif.append(gifRating);
 
-            $("#gifDisplay").prepend(gif);
-        }
-    })
+                $("#gifDisplay").prepend(gif);
+            }
+        })
 }
 
 function generateGifs2() {
@@ -105,32 +141,32 @@ function generateGifs2() {
         url: queryURL,
         method: "GET"
     })
-    .done(function(response) {
-        console.log(response);
-        $("#gifDisplay").empty();
-        var results = response.data;
-        if (results === "") {
-            alert("Sorry, no GIFs can be retrieved regarding this topic ):");
-        }
-        for (var i=0; i<results.length; i++) {
+        .done(function (response) {
+            console.log(response);
+            $("#gifDisplay").empty();
+            var results = response.data;
+            if (results === "") {
+                alert("Sorry, no GIFs can be retrieved regarding this topic ):");
+            }
+            for (var i = 0; i < results.length; i++) {
 
-            var gif = $("<div style='float:left'</div>");
-            $(gif).addClass("gifBox");
+                var gif = $("<div style='float:left'</div>");
+                $(gif).addClass("gifBox");
 
-            var gifImage = $("<img>");
-            $(gifImage).attr("src", results[i].images.fixed_height_small_still.url);
-            $(gifImage).attr("data-still", results[i].images.fixed_height_small_still.url);
-            $(gifImage).attr("data-animate", results[i].images.fixed_height_small.url);
-            $(gifImage).attr("data-state", "data-still");
-            $(gifImage).addClass("selection");
-            $(gif).append(gifImage);
+                var gifImage = $("<img>");
+                $(gifImage).attr("src", results[i].images.fixed_height_small_still.url);
+                $(gifImage).attr("data-still", results[i].images.fixed_height_small_still.url);
+                $(gifImage).attr("data-animate", results[i].images.fixed_height_small.url);
+                $(gifImage).attr("data-state", "data-still");
+                $(gifImage).addClass("selection");
+                $(gif).append(gifImage);
 
-            var gifRating = $("<p>").text("Rating: " + results[i].rating);
-            gif.append(gifRating);
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gif.append(gifRating);
 
-            $("#gifDisplay").prepend(gif);
-        }
-    })
+                $("#gifDisplay").prepend(gif);
+            }
+        })
 }
 
 function changeState() {
@@ -156,6 +192,8 @@ changeLimit();
 $(document).on("click", ".topic", generateGifs);
 
 $(document).on("click", ".selection", changeState);
+
+$("#movieButton").on("click", displayMovies);
 
 
 
